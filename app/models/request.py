38183463fe50f31e11aa808ac1a48240c29619f8
Lobur Yaroslav <lobur.iar@yandex.ru>
2022-03-13 19:03:32 +0300

    - выпилил неиспользуемые поля с датами

--- app/models/request.py
+++ app/models/request.py
@@ -1,6 +1,6 @@
 from db import Base
 from settings import settings
-from sqlalchemy import Column, DateTime, Integer, String, func
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.postgresql import JSONB
 from tornado_sqlalchemy import SQLAlchemy
 
@@ -13,5 +13,3 @@ class RequestModel(Base):
     id = Column(Integer, primary_key=True)
     rq_body_hash = Column(String, nullable=True)
     rq_body = Column(JSONB, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    modified_at = Column(DateTime(timezone=True), onupdate=func.now())
--- init_db/create_table.sql
+++ init_db/create_table.sql
@@ -2,9 +2,7 @@ CREATE TABLE request
 (
     id           SERIAL PRIMARY KEY,
     rq_body_hash VARCHAR,
-    rq_body      JSONB,
-    created_at   timestamp with time zone,
-    modified_at  timestamp with time zone
+    rq_body      JSONB
 );
 
 CREATE INDEX body_hash_idx ON request (rq_body_hash);
